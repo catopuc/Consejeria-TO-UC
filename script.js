1518b0b8168601e119ab9156046236a7ebce5100
catopuc <consejeriaacademicatouc@gmail.com>
2026-07-19 17:22:07 -0400

    Add function to calculate average of two grades

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function calcularPromedio(){
 
+    let n1 = parseFloat(
+        document.getElementById("nota1").value
+    );
+
+    let n2 = parseFloat(
+        document.getElementById("nota2").value
+    );
+
+    let promedio = (n1 + n2) / 2;
+
+    document.getElementById("resultado").innerHTML =
+        "Promedio: " + promedio.toFixed(1);
+
+}
