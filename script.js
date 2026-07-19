187d02768341f9fd9c027c54328468f48dfeff12
catopuc <consejeriaacademicatouc@gmail.com>
2026-07-19 18:16:33 -0400

    Refactor calcularPromedio function for clarity

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-function calcularPromedio(){
+function calcular(){
 
-    let n1 = parseFloat(
+    let n1 =
+    parseFloat(
         document.getElementById("nota1").value
-    );
+    ) || 0;
 
-    let n2 = parseFloat(
+    let n2 =
+    parseFloat(
         document.getElementById("nota2").value
-    );
+    ) || 0;
 
-    let promedio = (n1 + n2) / 2;
+    let promedio =
+    (n1+n2)/2;
 
-    document.getElementById("resultado").innerHTML =
-        "Promedio: " + promedio.toFixed(1);
-
+    document.getElementById(
+        "resultado"
+    ).innerText =
+    "Promedio: " +
+    promedio.toFixed(1);
 }
