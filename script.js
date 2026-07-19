0fe27035a7c5392edc3a6e8524d598fa0287f6c3
catopuc <consejeriaacademicatouc@gmail.com>
2026-07-19 19:37:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,41 @@ function calcular(){
 
     let n1 =
     parseFloat(
-        document.getElementById("nota1").value
+    document.getElementById("nota1").value
+    ) || 0;
+
+    let p1 =
+    parseFloat(
+    document.getElementById("pond1").value
     ) || 0;
 
     let n2 =
     parseFloat(
-        document.getElementById("nota2").value
+    document.getElementById("nota2").value
     ) || 0;
 
-    let promedio =
-    (n1+n2)/2;
+    let p2 =
+    parseFloat(
+    document.getElementById("pond2").value
+    ) || 0;
+
+    let n3 =
+    parseFloat(
+    document.getElementById("nota3").value
+    ) || 0;
+
+    let p3 =
+    parseFloat(
+    document.getElementById("pond3").value
+    ) || 0;
+
+    let resultado =
+    (n1*p1 + n2*p2 + n3*p3)/100;
 
     document.getElementById(
-        "resultado"
+    "resultado"
     ).innerText =
-    "Promedio: " +
-    promedio.toFixed(1);
+    "Nota Final: " +
+    resultado.toFixed(1);
+
 }
